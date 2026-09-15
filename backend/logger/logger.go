406aa9f9e33b6c6dd277115caee593b9ecc12cbc
Trần Minh Đức <wag06082005@gmail.com>
2026-09-15 12:08:31 +0700

    Log file in app.log

--- backend/logger/logger.go
+++ backend/logger/logger.go
@@ -1,19 +1,51 @@
 package logger
 
 import (
+	"os"
+
 	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 )
 
 var Log *zap.Logger
 
 func Init() {
-	var err error
 
-	Log, err = zap.NewProduction()
-
+	err := os.MkdirAll("logs", 0755)
 	if err != nil {
 		panic(err)
 	}
+
+	file, err := os.OpenFile(
+		"logs/app.log",
+		os.O_CREATE|os.O_APPEND|os.O_WRONLY,
+		0644,
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	encoderConfig := zap.NewProductionEncoderConfig()
+
+	encoder := zapcore.NewJSONEncoder(encoderConfig)
+
+	fileSyncer := zapcore.AddSync(file)
+
+	consoleSyncer := zapcore.AddSync(os.Stdout)
+
+	writeSyncer := zapcore.NewMultiWriteSyncer(
+		fileSyncer,
+		consoleSyncer,
+	)
+
+	core := zapcore.NewCore(
+		encoder,
+		writeSyncer,
+		zap.InfoLevel,
+	)
+
+	Log = zap.New(core)
+
 }
 
 func Sync() {
