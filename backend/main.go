18548b351e14fe7d81a5e16b1116d17f8774612f
Trần Minh Đức <wag06082005@gmail.com>
2026-09-15 11:49:10 +0700

    route in routes

--- backend/main.go
+++ backend/main.go
@@ -1,18 +1,14 @@
 package main
 
 import (
-	//"log"
 	"net/http"
 	"os"
 
 	"go.uber.org/zap"
 
 	"backend/config"
-	"backend/controllers"
 	"backend/logger"
-	"backend/repositories"
 	"backend/routes"
-	"backend/services"
 
 	"github.com/gin-gonic/gin"
 )
@@ -24,20 +20,14 @@ func main() {
 
 	supabaseClient := config.InitSupabaseClient()
 
-	productRepo := repositories.NewSupabaseProductRepository(supabaseClient)
-	productService := services.NewProductService(productRepo)
-	productController := controllers.NewProductController(productService)
-
 	router := gin.Default()
+	routes.SetupRoutes(router, supabaseClient)
 
 	router.GET("/health", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"status": "healthy",
 		})
 	})
-
-	api := router.Group("/api")
-	routes.ProductRoutes(api, productController)
 
 	port := os.Getenv("PORT")
 	if port == "" {
--- backend/routes/routes.go
+++ backend/routes/routes.go
@@ -2,9 +2,23 @@ package routes
 
 import (
 	"backend/controllers"
+	"backend/repositories"
+	"backend/services"
 
 	"github.com/gin-gonic/gin"
+	"github.com/supabase-community/supabase-go"
 )
+
+func SetupRoutes(r *gin.Engine, supabaseClient *supabase.Client) {
+
+	api := r.Group("/api")
+
+	productRepo := repositories.NewSupabaseProductRepository(supabaseClient)
+	productService := services.NewProductService(productRepo)
+	productController := controllers.NewProductController(productService)
+
+	ProductRoutes(api, productController)
+}
 
 func ProductRoutes(router *gin.RouterGroup, controller *controllers.ProductController) {
 	products := router.Group("/products")
