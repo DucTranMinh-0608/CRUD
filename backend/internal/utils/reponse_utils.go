0eb1e90d066d289c5775184a02e3e943fede75f6
Trần Minh Đức <wag06082005@gmail.com>
2026-09-16 12:02:22 +0700

    fix

--- backend/internal/utils/reponse_utils.go
+++ backend/internal/utils/reponse_utils.go
@@ -1,41 +1,35 @@
 package utils
 
 import (
-	//"net/http"
-
-	"backend/internal/models"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 func InvalidDataResponse(c *gin.Context, err error) {
-	c.JSON(400, models.APIResponse{
-		Success: false,
-		Message: "Dữ liệu không hợp lệ",
-		Error:   err.Error(),
+	c.JSON(http.StatusOK, gin.H{
+		"Message": "Dữ liệu không hợp lệ",
+		"Error":   err.Error(),
 	})
 }
 
 func NoConnectDataResponse(c *gin.Context, err error) {
-	c.JSON(500, models.APIResponse{
-		Success: false,
-		Message: "Không ghi dữ liệu được vào database",
-		Error:   err.Error(),
+	c.JSON(http.StatusInternalServerError, gin.H{
+		"Message": "Không ghi dữ liệu được vào database",
+		"Error":   err.Error(),
 	})
 }
 
 func NotFoundDataResponse(c *gin.Context, err error) {
-	c.JSON(404, models.APIResponse{
-		Success: false,
-		Message: "Không tìm thấy dữ liệu",
-		Error:   err.Error(),
+	c.JSON(http.StatusNotFound, gin.H{
+		"Message": "Không tìm thấy dữ liệu",
+		"Error":   err.Error(),
 	})
 }
 
 func SuccessResponse(c *gin.Context, s string, products interface{}) {
-	c.JSON(200, models.APIResponse{
-		Success: true,
-		Message: s + " thành công",
-		Data:    products,
+	c.JSON(http.StatusOK, gin.H{
+		"Message": s + " thành công",
+		"Data":    products,
 	})
 }
